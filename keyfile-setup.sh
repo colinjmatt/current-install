198183bf2f71de0273db5a27cb1875de626b4293
Colin Matthews <colinjmatt@users.noreply.github.com>
2019-04-11 13:10:57 +0100

    Standardised variables to lowercase

--- keyfile-setup.sh
+++ keyfile-setup.sh
@@ -3,8 +3,8 @@
 clear
 lsblk
 echo ""
-KEYDRIVE="$1"
-if [[ -z "$KEYDRIVE" ]]; then
+keydrive="$1"
+if [[ -z "$keydrive" ]]; then
     echo -n "Please enter the USB drive identifier (eg: sdb) "
     read -r USB < /dev/tty
 fi
@@ -12,8 +12,8 @@ fi
 clear
 lsblk
 echo ""
-ENCRYPTEDDRIVE="$1"
-if [[ -z "$ENCRYPTEDDRIVE" ]]; then
+encrypteddrive="$1"
+if [[ -z "$encrypteddrive" ]]; then
     echo -n "Please enter the encrypted drive identifier (eg: sda2) "
     read -r CRYPT < /dev/tty
 fi
@@ -21,24 +21,24 @@ fi
 sudo pacman -S --noconfirm dosfstools parted
 
 sudo mkdir -p /mnt/usbkey
-sudo parted -s /dev/"$KEYDRIVE" mklabel gpt
-sudo parted -s /dev/"$KEYDRIVE" mkpart FAT32
-sudo mkfs.vfat -I /dev/"$KEYDRIVE"
-sudo mount /dev/"$KEYDRIVE" /mnt/usbkey
+sudo parted -s /dev/"$keydrive" mklabel gpt
+sudo parted -s /dev/"$keydrive" mkpart FAT32
+sudo mkfs.vfat -I /dev/"$keydrive"
+sudo mount /dev/"$keydrive" /mnt/usbkey
 
-KEYDRIVEUUID=$(blkid /dev/""$KEYDRIVE"" -o value | sed -n "/msdos/{n;p}")
+keydriveUUID=$(blkid /dev/""$keydrive"" -o value | sed -n "/msdos/{n;p}")
 sudo dd bs=1024 count=4 if=/dev/urandom of=/mnt/usbkey/crypt.key iflag=fullblock
 
 clear
 echo "Encrypted volume password entry"
 echo "========================================================================="
 echo ""
-sudo cryptsetup luksAddKey /dev/"$ENCRYPTEDDRIVE" /mnt/usbkey/crypt.key --key-slot 1
+sudo cryptsetup luksAddKey /dev/"$encrypteddrive" /mnt/usbkey/crypt.key --key-slot 1
 
 sudo sed -i -e "s/MODULES=\"/MODULES=\"nls_cp437 vfat\ /g" /etc/mkinitcpio.conf
 mkinitcpio -p linux linux-rt-bfq
 
-sudo sed -i -e "s/options\ /options\ cryptkey=UUID=""$KEYDRIVEUUID"":vfat:\/crypt.key\ /g" /boot/loader/entries/arch.conf
+sudo sed -i -e "s/options\ /options\ cryptkey=UUID=""$keydriveuuid"":vfat:\/crypt.key\ /g" /boot/loader/entries/arch.conf
 
 sudo umount /mnt/usbkey
 sudo rm -rf /mnt/usbkey
