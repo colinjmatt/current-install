b7f6b55d643ed496af25919fa2d9c40ca932ef42
Colin Matthews <colin@matthews-co.uk>
2020-02-24 17:58:58 +0000

    No longer using rsync

--- Configs/backup.sh
+++ Configs/backup.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
-if [[ ! -d /mnt/offsite-hetzner/Backups ]] && ping -q -c 1 "$backupuser".your-storagebox.de >/dev/null; then
-    sshfs -o allow_other,reconnect,uid=0,gid=100,umask=003,ServerAliveInterval=15,ServerAliveCountMax=3 "$backupuser".your-storagebox.de:/ /mnt/offsite-hetzner
-elif [[ -d /mnt/offsite-hetzner/Backups ]] && ping -q -c 1 "$backupuser".your-storagebox.de >/dev/null; then
-    :
-else
-   exit 1
+user=""
+server=""
+port=""
+remotemount=""
+localmount=""
+directory=""
+passphrasefilelocation=""
+backupname=""
+days="90"
+
+if [[ ! -d "$localmount" ]]; then
+  mkdir -p "$localmount"
 fi
 
-rsync -aR --delete \
-    /home \
-    /root \
-    /etc \
-    /usr/local/bin/ \
-    /usr/share/icons/ \
-    /usr/share/themes/ \
-    --include '/usr/share/themes/*.keep' \
-    --include '/usr/share/icons/*.keep' \
-    --exclude '/home/colin/*cache' \
-    --exclude '/home/colin/.mozilla' \
-    --exclude '/usr/share/themes/*' \
-    --exclude '/usr/share/icons/*' \
-    --exclude '/home/colin/.config/discord/' \
-    --exclude '/home/colin/.config/OCS-Store' \
-    --exclude '/home/colin/.config/Opendesktop App' \
-    /mnt/Backup/"$hostname"
+if [[ ! -d "$localmount""$directory" ]]; then
+  sshfs -p "$port" "$user"@"$server":"$remotemount" "$localmount"
+elif [[ -d "$localmount""$directory" ]]; then
+  :
+else
+  exit 1
+fi
+
+if [[ ! -d "$localmount""$directory" ]]; then
+  mkdir -p "$localmount""$directory"
+fi
 
 tar -zc \
-    /mnt/Backup/"$hostname"/ | \
+  --exclude='/home/colin/*cache' \
+  --exclude='/home/colin/.mozilla' \
+  --exclude='/home/colin/.config/discord/' \
+  --exclude='/home/colin/.config/OCS-Store' \
+  --exclude='/home/colin/.config/Opendesktop App' \
+  /home \
+  /root \
+  /etc \
+  /usr/local/bin/ \
+  /usr/share/icons/*.keep \
+  /usr/share/themes/*.keep | \
 gpg -se \
-    -r admin \
-    --batch \
-    --yes \
-    --pinentry-mode loopback \
-    --passphrase-file /root/.gpg-admin \
-    -o /mnt/offsite-hetzner/Backups/"$hostname"/"$hostname"-"$(date +%Y-%m-%d)".tar.gz.gpg
+  -r admin \
+  --batch \
+  --yes \
+  --pinentry-mode loopback \
+  --passphrase-file "$passphrasefilelocation" \
+  -o "$localmount""$directory""$backupname"/"$backupname"-"$(date +%Y-%m-%d)".tar.gz.gpg
 
-rm -rf /tmp/"$hostname"
-umount /mnt/offsite-hetzner/
+find "$localmount""$directory""$backupname"/"$backupname"* -mtime +"$days" -exec rm {} \;
+
+umount "$localmount"
