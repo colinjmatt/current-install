0bbed38287305b4b1f3507eee12f627590456b7b
Colin Matthews <colin@matthews-co.uk>
2020-09-02 20:11:04 +0100

    Update part-3.sh

--- part-3.sh
+++ part-3.sh
@@ -68,7 +68,6 @@ gpg --keyserver pool.sks-keyservers.net \
     ABAF11C65A2970B130ABE3C479BE3E4300411886 \
     647F28654894E3BD457199BE38DBBDC86092693E
 yay -S --noconfirm \
-  linux-rt-bfq-dev \
   google-chrome \
   parsec-bin \
   xfce4-volumed-pulse mugshot \
