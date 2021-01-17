b54c506e63e8b25d5dafc9e191e5046bc00206f4
Colin Matthews <colin@matthews-co.uk>
2021-01-17 12:30:00 +0000

    Update backup.sh
    Minor updates

--- Configs/backup.sh
+++ Configs/backup.sh
@@ -13,30 +13,27 @@ if [[ ! -d "$localmount" ]]; then
   mkdir -p "$localmount"
 fi
 
-if [[ ! -d "$localmount""$directory" ]]; then
+if ! mountpoint "$localmount" >/dev/null; then
   sshfs -p "$port" "$user"@"$server":"$remotemount" "$localmount"
-elif [[ -d "$localmount""$directory" ]]; then
+elif mountpoint "$localmount" >/dev/null; then
   :
 else
   exit 1
 fi
 
-if [[ ! -d "$localmount""$directory" ]]; then
-  mkdir -p "$localmount""$directory"
+if [[ ! -d "$localmount""$directory""$backupname" ]]; then
+  mkdir -p "$localmount""$directory""$backupname"
 fi
 
 tar -zc \
   --exclude='/home/colin/*cache' \
-  --exclude='/home/colin/.mozilla' \
   --exclude='/home/colin/.config/discord/' \
   --exclude='/home/colin/.config/OCS-Store' \
   --exclude='/home/colin/.config/Opendesktop App' \
   /home \
   /root \
   /etc \
-  /usr/local/bin/ \
-  /usr/share/icons/*.keep \
-  /usr/share/themes/*.keep | \
+  /usr/local/bin/ | \
 gpg -se \
   -r admin \
   --batch \
