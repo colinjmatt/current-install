29c9dc912b2d23acd5e967f3260377fc7d81f092
Colin Matthews <colin@matthews-co.uk>
2021-01-17 20:27:59 +0000

    Update part-1.sh
    Changed the way mirrors are obtained

--- part-1.sh
+++ part-1.sh
@@ -2,45 +2,49 @@
 # BACKUP /home/user/    BEFORE REINSTALLING!!
 # BACKUP /etc/libvirt/  BEFORE REINSTALLING!!
 
-# Set time & keyboard map
+# Set keyboard map & time sync
+loadkeys uk
 timedatectl set-ntp true
-loadkeys uk.map.gz
 
-# Sort the mirrorlist so downloads are fast enough
-grep --no-group-separator -A1 "United Kingdom" /etc/pacman.d/mirrorlist > /mirrorlist
-cat /mirrorlist > /etc/pacman.d/mirrorlist
+# Sort the mirrorlist so downloads are fast enough and from teh correct location
+reflector --country 'United Kingdom' --latest 200 --age 24 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Create partitions, create and open encrypted volume and mount all partitions
-parted -s /dev/nvme0n1 mklabel gpt
-parted -s /dev/nvme0n1 mkpart ESP fat32 2048s 512MiB
-parted -s /dev/nvme0n1 set 1 boot on
-parted -s /dev/nvme0n1 mkpart primary ext4 512MiB 100%
-mkfs.vfat -F32 /dev/nvme0n1p1
+parted -s /dev/nvme1n1 mklabel gpt
+parted -s /dev/nvme1n1 mkpart ESP fat32 2048s 512MiB
+parted -s /dev/nvme1n1 set 1 boot on
+parted -s /dev/nvme1n1 mkpart primary ext4 512MiB 100%
+mkfs.vfat -F32 /dev/nvme1n1p1
 
-cryptsetup luksFormat /dev/nvme0n1p2
-cryptsetup luksOpen /dev/nvme0n1p2 nvme0n1p2-crypt
+cryptsetup luksFormat /dev/nvme1n1p2
+cryptsetup luksOpen /dev/nvme1n1p2 nvme1n1p2-crypt
 
-pvcreate /dev/mapper/nvme0n1p2-crypt
-vgcreate vg0 /dev/mapper/nvme0n1p2-crypt
-lvcreate -L 32G vg0 -n swap
-lvcreate -l 100%FREE vg0 -n root
+pvcreate /dev/mapper/nvme1n1p2-crypt
+vgcreate vg0 /dev/mapper/nvme1n1p2-crypt
+lvcreate -L 64G vg0 -n swap
+lvcreate -L 128G vg0 -n root
+lvcreate -l 100%FREE vg0 -n vms
 mkswap /dev/mapper/vg0-swap
 mkfs.ext4 /dev/mapper/vg0-root
+mkfs.ext4 /dev/mapper/vg0-vms
 
 mount /dev/mapper/vg0-root /mnt
 mkdir /mnt/boot
-mount /dev/nvme0n1p1 /mnt/boot
+mount /dev/nvme1n1p1 /mnt/boot
 swapon /dev/mapper/vg0-swap
 
 # Install base system
 pacstrap /mnt base base-devel intel-ucode linux linux-firmware lvm2 systemd-resolvconf openssh wget nano git
+
+mkdir /mnt/mnt/VMs
+mount /dev/mapper/vg0-vms /mnt/mnt/VMs
 
 # Generate fstab for filesystem mounts and add /tmp as ram drive
 genfstab -pU /mnt >>/mnt/etc/fstab
 echo "tmpfs	/tmp	tmpfs	defaults,noatime,mode=1777	0	0" >>/mnt/etc/fstab
 
 # Copy mirrorlist to installed base system
-cat /mirrorlist >/mnt/etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist >/mnt/etc/pacman.d/mirrorlist
 
 # Chroot in
 arch-chroot /mnt
