d3b6c7ce5bc349a688c5b515f3dd2d816d34367a
Colin Matthews <colin@matthews-co.uk>
2021-01-17 20:27:14 +0000

    Update htpc_part-1.sh
    Changed the way the mirrorlist is obtained

--- htpc_part-1.sh
+++ htpc_part-1.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 # BACKUP /home/user/    BEFORE REINSTALLING!!
 
-# Set time & keyboard map
+# Set keyboard map & time sync
+loadkeys uk
 timedatectl set-ntp true
-loadkeys uk.map.gz
 
-# Sort the mirrorlist so downloads are fast enough
-grep --no-group-separator -A1 "United Kingdom" /etc/pacman.d/mirrorlist > /mirrorlist
-cat /mirrorlist > /etc/pacman.d/mirrorlist
+# Sort the mirrorlist so downloads are fast enough and from teh correct location
+reflector --country 'United Kingdom' --latest 200 --age 24 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Create partitions, create and open encrypted volume and mount all partitions
-parted -s /dev/sda mklabel gpt
-parted -s /dev/sda mkpart ESP fat32 2048s 512MiB
-parted -s /dev/sda set 1 boot on
-parted -s /dev/sda mkpart primary ext4 512MiB 16896MiB
-parted -s /dev/sda mkpart primary ext4 16896MiB 100%
+parted -s /dev/nvme0n1 mklabel gpt
+parted -s /dev/nvme0n1 mkpart ESP fat32 2048s 512MiB
+parted -s /dev/nvme0n1 set 1 boot on
+parted -s /dev/nvme0n1 mkpart primary ext4 512MiB 16896MiB
+parted -s /dev/nvme0n1 mkpart primary ext4 16896MiB 100%
 
-mkfs.vfat -F32 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
+mkfs.vfat -F32 /dev/nvme0n1p1
+mkswap /dev/nvme0n1p2
+mkfs.ext4 /dev/nvme0n1p3
 
-mount /dev/sda3 /mnt
+mount /dev/nvme0n1p3 /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-swapon /dev/sda2
+mount /dev/nvme0n1p1 /mnt/boot
+swapon /dev/nvme0n1p2
 
 # Install base system
 pacstrap /mnt base base-devel amd-ucode linux linux-firmware systemd-resolvconf openssh wget nano git
@@ -33,7 +32,7 @@ genfstab -pU /mnt >>/mnt/etc/fstab
 echo "tmpfs	/tmp	tmpfs	defaults,noatime,mode=1777	0	0" >>/mnt/etc/fstab
 
 # Copy mirrorlist to installed base system
-cat /mirrorlist >/mnt/etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist >/mnt/etc/pacman.d/mirrorlist
 
 # Chroot in
 arch-chroot /mnt
