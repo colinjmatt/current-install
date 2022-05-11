7645070fb358bf3aa096c933e1eedf9c4719f629
Colin Matthews <colinjmatt@users.noreply.github.com>
2022-05-11 19:25:47 +0100

    Update part-1.sh
    Updated to AMD

--- part-1.sh
+++ part-1.sh
@@ -10,17 +10,17 @@ timedatectl set-ntp true
 reflector --country 'United Kingdom' --protocol https --latest 50 --fastest 8 --age 24 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Create partitions, create and open encrypted volume and mount all partitions
-parted -s /dev/nvme1n1 mklabel gpt
-parted -s /dev/nvme1n1 mkpart ESP fat32 2048s 512MiB
-parted -s /dev/nvme1n1 set 1 boot on
-parted -s /dev/nvme1n1 mkpart primary ext4 512MiB 100%
-mkfs.vfat -F32 /dev/nvme1n1p1
+parted -s /dev/nvme0n1 mklabel gpt
+parted -s /dev/nvme0n1 mkpart ESP fat32 2048s 512MiB
+parted -s /dev/nvme0n1 set 1 boot on
+parted -s /dev/nvme0n1 mkpart primary ext4 512MiB 100%
+mkfs.vfat -F32 /dev/nvme0n1p1
 
-cryptsetup luksFormat /dev/nvme1n1p2
-cryptsetup luksOpen /dev/nvme1n1p2 nvme1n1p2-crypt
+cryptsetup luksFormat /dev/nvme0n1p2
+cryptsetup luksOpen /dev/nvme0n1p2 nvme1n1p2-crypt
 
-pvcreate /dev/mapper/nvme1n1p2-crypt
-vgcreate vg0 /dev/mapper/nvme1n1p2-crypt
+pvcreate /dev/mapper/nvme0n1p2-crypt
+vgcreate vg0 /dev/mapper/nvme0n1p2-crypt
 lvcreate -L 64G vg0 -n swap
 lvcreate -L 128G vg0 -n root
 lvcreate -l 100%FREE vg0 -n vms
@@ -30,11 +30,11 @@ mkfs.ext4 /dev/mapper/vg0-vms
 
 mount /dev/mapper/vg0-root /mnt
 mkdir /mnt/boot
-mount /dev/nvme1n1p1 /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 swapon /dev/mapper/vg0-swap
 
 # Install base system
-pacstrap /mnt base base-devel intel-ucode linux linux-firmware lvm2 systemd-resolvconf openssh wget nano git
+pacstrap /mnt base base-devel amd-ucode linux linux-firmware lvm2 systemd-resolvconf openssh wget nano git
 
 mkdir /mnt/mnt/VMs
 mount /dev/mapper/vg0-vms /mnt/mnt/VMs
