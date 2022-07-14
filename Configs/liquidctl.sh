ebffb1abeb023081fd179a26e98c4700673baaec
Colin Matthews <colin@matthews.uk.net>
2022-07-14 21:40:25 +0100

    Remove liquidctl

--- Configs/liquidctl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-liquidctl initialize all
-
-liquidctl --match kraken set pump speed 20 60 40 100
-liquidctl --match kraken set fan speed  20 25  30 35  40 50  45 75  55 100
-liquidctl set sync color fixed ff0000
