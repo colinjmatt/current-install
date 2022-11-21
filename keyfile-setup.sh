e01167681561ae56efe750c6ae47c70ea85ef0ae
Colin Matthews <colin@Five-Hundred.local>
2022-11-21 22:39:27 +0000

    Update keyfile-setup.sh
    Now uses ext4

--- keyfile-setup.sh
+++ keyfile-setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 clear
 lsblk
 echo ""
 keydrive="$1"
 if [[ -z "$keydrive" ]]; then
     echo -n "Please enter the USB drive identifier (eg: sdb) "
-    read -r USB < /dev/tty
+    read -r keydrive < /dev/tty
 fi
 
 clear
@@ -15,19 +14,18 @@ echo ""
 encrypteddrive="$1"
 if [[ -z "$encrypteddrive" ]]; then
     echo -n "Please enter the encrypted drive identifier (eg: sda2) "
-    read -r CRYPT < /dev/tty
+    read -r encrypteddrive < /dev/tty
 fi
 
-pacman -S --noconfirm dosfstools parted
+pacman -S --noconfirm parted
 
 mkdir -p /mnt/usbkey
 parted -s /dev/"$keydrive" mklabel gpt
-parted -s /dev/"$keydrive" mkpart FAT32
-mkfs.vfat -I /dev/"$keydrive"
+mkfs.ext4 /dev/"$keydrive"
 mount /dev/"$keydrive" /mnt/usbkey
 
-keydriveUUID=$(blkid /dev/""$keydrive"" -o value | head -n1)
-dd bs=1024 count=4 if=/dev/urandom of=/mnt/usbkey/crypt.key iflag=fullblock
+keydriveUUID=$(blkid /dev/"$keydrive" -o value | head -n1)
+dd bs=4096 count=4 if=/dev/urandom of=/mnt/usbkey/crypt.key iflag=fullblock
 
 clear
 echo "Encrypted volume password entry"
@@ -39,7 +37,7 @@ cryptsetup luksAddKey /dev/"$encrypteddrive" /mnt/usbkey/crypt.key --key-slot 1
 # sed -i -e "s/MODULES=\"/MODULES=\"nls_cp437 vfat\ /g" /etc/mkinitcpio.conf
 # mkinitcpio -P
 
-sed -i -e "s/options\ /options\ cryptkey=UUID=""$keydriveUUID"":vfat:\/crypt.key\ /g" /boot/loader/entries/*.conf
+sed -i -e "s/options\ /options\ cryptkey=UUID=""$keydriveUUID"":ext4:\/crypt.key\ /g" /boot/loader/entries/*.conf
 
 umount /mnt/usbkey
 rm -rf /mnt/usbkey
