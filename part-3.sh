c43f9d8fe2df579010840b57ff33b9717f1081a8
colinjmatt <colinjmatt@users.noreply.github.com>
2024-06-01 13:48:00 +0100

    Added headset control apps

--- part-3.sh
+++ part-3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 user="user" # single user only
-yayuser=""
+paruuser=""
 hostname="hostname"
 machine="machine" # Friendly computer name for airplay stuff
 domain="x.local"
@@ -40,7 +40,7 @@ pacman -S --noconfirm \
   mesa mesa-vdpau  \
   nvidia-settings nvidia-utils \
   neofetch net-tools network-manager-applet networkmanager networkmanager-openvpn noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nss-mdns ntfs-3g \
-  p7zip paprefs parted pasystray pavucontrol polkit poppler poppler-data pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-x11-bell pipewire-zeroconf python-psutil \
+  p7zip paprefs parted pasystray pavucontrol polkit poppler poppler-data pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-x11-bell pipewire-zeroconf python-psutil python-pyusb \
   qemu-desktop \
   reflector rsync \
   sane seahorse shairplay slock speedtest-cli sshfs swtpm sysstat \
@@ -65,12 +65,12 @@ sed -i -e "\
 
 # Install AUR helper of the month (as a non-priviledged user) and install AUR software
 ( cd /tmp || return
-su "$yayuser" -P -c 'git clone https://aur.archlinux.org/paru-bin.git'
+su "$paruuser" -P -c 'git clone https://aur.archlinux.org/paru-bin.git'
 cd /tmp/paru-bin || return
-su "$yayuser" -P -c 'makepkg -si --noconfirm; \
+su "$paruuser" -P -c 'makepkg -si --noconfirm; \
   paru -S --noconfirm \
   brother-dcp-9020cdw brscan4 \
-  heroic-games-launcher-bin \
+  headsetcontrol headset-charge-indicator heroic-games-launcher-bin \
   mugshot \
   numix-circle-icon-theme-git numix-icon-theme-git \
   openrgb \
@@ -88,7 +88,7 @@ echo "[multilib]" >>/etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >>/etc/pacman.conf
 pacman -Syu --noconfirm
 pacman -S lib32-fontconfig lib32-libva-mesa-driver lib32-libnm lib32-mesa lib32-mesa-utils lib32-mesa-vdpau lib32-nvidia-utils lib32-systemd steam --noconfirm
-su "$yayuser" -P -c 'paru -S --noconfirm proton-ge-custom-bin protonup-qt-bin steamtinkerlaunch && steamtinkerlaunch compat add'
+su "$paruuser" -P -c 'paru -S --noconfirm proton-ge-custom-bin protonup-qt-bin steamtinkerlaunch && steamtinkerlaunch compat add'
 sed -i -e "s/\#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/g" /etc/locale.gen
 locale-gen
 
@@ -164,6 +164,15 @@ mkdir -p /home/"$user"/.config/autostart
 # RGB stuff
 cat ./Configs/OpenRGB.desktop >/home/"$user"/.config/autostart/OpenRGB.desktop
 
+# Headset Control
+cat ./Configs/HeadsetControl.desktop >/home/"$user"/.config/autostart/HeadsetControl.desktop
+cat ./Configs/Arctis7PlusChatMix.py >/usr/local/bin/Arctis7PlusChatMix.py
+cat ./Configs/99arctis7plus.rules /etc/udev/rules.d/99arctis7plus.rules
+cat ./Configs/Arctis7PlusChatMix.desktop >/home/"$user"/.config/autostart/Arctis7PlusChatMix.desktop
+
+sed -i -e "s/$user/""$user""/g" /etc/udev/rules.d/91-steelseries-arctis-7p.rules
+su "$user" -P -c 'systemctl --user enable arctis7pcm.service'
+
 # Enable backups
 cat ./Configs/backup.sh >/usr/local/bin/backup.sh
 read -n 1 -s -r -p "Switch to another TTY and complete the backup script variables. Press any key to continue..."
@@ -194,8 +203,9 @@ cat ./Configs/xorg.conf >/etc/X11/xorg.conf
 # Set permissions
 chmod +x -R \
   /usr/local/bin/*
+
 chown -R "$user":"$user" \
-  /home/"$user"/.config/autostart
+  /home/"$user"/.config/autostart \
   /home/"$user"/.local/state/wireplumber/default-nodes
 
 # Disable initial networking services
