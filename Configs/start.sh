4c7e5c7fa8b7017f67f7f151a426c2f5fbfa5f49
colinjmatt <colinjmatt@users.noreply.github.com>
2024-07-04 22:03:19 +0100

    Update start.sh
    Added nvidia-persistenced

--- Configs/start.sh
+++ Configs/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cpupower frequency-set -g performance
 
-systemctl stop vncserver-x11-serviced
-systemctl stop lightdm.service
+systemctl stop \
+    vncserver-x11-serviced.service \
+    lightdm.service \
+    nvidia-persistenced.service
 
 virsh nodedev-detach pci_0000_09_00_0
 virsh nodedev-detach pci_0000_09_00_1
