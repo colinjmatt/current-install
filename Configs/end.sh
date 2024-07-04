baf15351730d2138fcf359337850ca8302f3be87
colinjmatt <colinjmatt@users.noreply.github.com>
2024-07-04 22:03:14 +0100

    Update end.sh
    Added nvidia-persistenced

--- Configs/end.sh
+++ Configs/end.sh
@@ -2,7 +2,9 @@
 virsh nodedev-reattach pci_0000_09_00_0
 virsh nodedev-reattach pci_0000_09_00_1
 
-systemctl start lightdm.service
-systemctl start vncserver-x11-serviced
+systemctl start \
+    lightdm.service \
+    vncserver-x11-serviced.service \
+    nvidia-persistenced.service
 
 cpupower frequency-set -g powersave
