5f0129cd6e0e77b9c8b850c253bf549fbb614e2a
colinjmatt <colinjmatt@users.noreply.github.com>
2024-07-26 14:18:58 +0100

    Update paccache.sh
    I want verbosity

--- Configs/paccache.sh
+++ Configs/paccache.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Removing old pacman package versions from the cache, but keeping the 2 most recent."
 paccache -rk2
-paccache -ruk1
+echo "Removing all pacman packages from the cache that are no longer installed"
+paccache -ruk0
 
+
+echo "Removing old paru package versions from the cache, but keeping the 2 most recent."
 paccache -rk2 -c /home/$user/.cache/paru/*/
+echo "Removing all paru packages from the cache that are no longer installed"
 paccache -ruk0 /home/$user/.cache/paru/*/
