dc0d9125de841fdfc8a3491c28067e0499e3bf10
colinjmatt <colinjmatt@users.noreply.github.com>
2025-01-04 11:24:12 +0000

    Update paccache.sh
    Better layout of text

--- Configs/paccache.sh
+++ Configs/paccache.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "Removing old pacman package versions from the cache, but keeping the 2 most recent."
 paccache -rk2
+echo ""
 echo "Removing all pacman packages from the cache that are no longer installed"
 paccache -ruk0
-
+echo ""
 
 echo "Removing old paru package versions from the cache, but keeping the 2 most recent."
-paccache -rk2 -c /home/$user/.cache/paru/*/
+paccache -rk2 -c /home/"$user"/.cache/paru/*/
+echo ""
 echo "Removing all paru packages from the cache that are no longer installed"
-paccache -ruk0 /home/$user/.cache/paru/*/
+paccache -ruk0 /home/"$user"/.cache/paru/*/
+echo ""
