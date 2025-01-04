7803c3ce62dfe1b04ebbb9a07a54f382798289ec
colinjmatt <colinjmatt@users.noreply.github.com>
2025-01-04 11:23:22 +0000

    Update end.sh
    GPU properly attaches/detaches

--- Configs/end.sh
+++ Configs/end.sh
@@ -2,6 +2,11 @@
 virsh nodedev-reattach pci_0000_09_00_0
 virsh nodedev-reattach pci_0000_09_00_1
 
+modprobe nvidia_drm
+modprobe nvidia_modeset
+modprobe nvidia_uvm
+modprobe nvidia
+
 systemctl start \
     lightdm.service \
     vncserver-x11-serviced.service \
