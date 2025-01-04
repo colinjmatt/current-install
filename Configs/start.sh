14ca0b63c80228449613f05a745a1c58e3e63c10
colinjmatt <colinjmatt@users.noreply.github.com>
2025-01-04 11:23:27 +0000

    Update start.sh
    GPU properly attaches/detaches

--- Configs/start.sh
+++ Configs/start.sh
@@ -6,5 +6,10 @@ systemctl stop \
     lightdm.service \
     nvidia-persistenced.service
 
+modprobe -r nvidia_drm
+modprobe -r nvidia_modeset
+modprobe -r nvidia_uvm
+modprobe -r nvidia
+
 virsh nodedev-detach pci_0000_09_00_0
 virsh nodedev-detach pci_0000_09_00_1
